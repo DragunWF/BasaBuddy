53b64c47a9229207fff57a68ad628576c193c207
DragunWF <plarisanmarc2@gmail.com>
2025-07-21 22:43:27 +0800

    Update Stack Navigator

--- components/Navigation/StackNavigator.jsx
+++ components/Navigation/StackNavigator.jsx
@@ -1,12 +1,14 @@
 import { createStackNavigator } from "@react-navigation/stack";
 
 import ChatScreen from "../../screens/ChatScreen";
+import HomeScreen from "../../screens/HomeScreen";
 
 const Stack = createStackNavigator();
 
 function StackNavigator() {
   return (
     <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Chat" component={ChatScreen} />
     </Stack.Navigator>
   );
