8d541e41d9386e71c33119357fa3ed2d835dc790
DragunWF <plarisanmarc2@gmail.com>
2025-07-21 14:25:47 +0800

    Create ChatBubble component

---
+++ screens/ChatScreen.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, View, Text, FlatList } from "react-native";
+
+import ChatBubble from "../components/Chat/ChatBubble";
+
+function ChatScreen() {
+  return (
+    <View>
+      <Text>This is for testing the chat screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default ChatScreen;
