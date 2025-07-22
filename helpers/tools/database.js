f06c4fd61c269507a3eb1d3e8fec0afcd9f79f1c
DragunWF <plarisanmarc2@gmail.com>
2025-07-22 16:18:14 +0800

    Update database.js

--- helpers/tools/database.js
+++ helpers/tools/database.js
@@ -5,8 +5,10 @@ const database = SQLite.openDatabaseAsync("basabuddy.db");
 // The logins table will primarily be used for tracking the streak
 export function init() {
   return database.runAsync(`
-CREATE TABLE readingStreak (
+CREATE TABLE booksRead (
     id INTEGER PRIMARY KEY NOT NULL,
-    lastReadDate TEXT KEY NOT NULL
+    title TEXT NOT NULL,
+    author TEXT NOT NULL,
+    description TEXT
 )`);
 }
